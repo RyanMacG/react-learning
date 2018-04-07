ce2b95927640d4a20a6a369c40c36309ec2c3710
Ryan MacGillivray <rmacg89@gmail.com>
2018-04-07 17:38:05 +0100

    Add global typography styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-import Footer from './components/Footer/index';
-import Header from './components/Header/index';
+import React, { Component } from 'react'
+import loremIpsum from 'lorem-ipsum'
+import './App.css'
+import './styles/typography.css'
+import Footer from './components/Footer/index'
+import Header from './components/Header/index'
+
+function lorem() {
+  const output = loremIpsum({
+    count: 5,
+    units: 'paragraphs',
+    format: 'html'
+  })
+
+  return { __html: output }
+}
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
         <Header />
-        <main>
+        <main className="content">
+          <div dangerouslySetInnerHTML={lorem()} />
         </main>
         <Footer />
       </div>
-    );
+    )
   }
 }
